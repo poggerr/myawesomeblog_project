ae6926172cc9bbaa206d4bad45f1fc3a1bba2bc7
Egor <etor0pov@yandex.ru>
2020-12-02 14:19:26 +0300

    Delete secret key

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u92p&+zqi+lt#=an%eez09^h7t61ik*)a0ym=j!&+0_tw%q@m('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
